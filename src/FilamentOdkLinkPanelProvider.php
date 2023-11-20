d38b3842ce173c5cd77ffa0c5c3921e3823fd732
dave-mills <dave-mills@users.noreply.github.com>
2023-11-20 18:24:57 +0000

    Fix styling

--- src/FilamentOdkLinkPanelProvider.php
+++ src/FilamentOdkLinkPanelProvider.php
@@ -2,12 +2,12 @@
 
 namespace Stats4sd\FilamentOdkLink;
 
+use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Http\Middleware\Authenticate;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -15,9 +15,6 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Stats4sd\FilamentOdkLink\Filament\Resources\DatasetResource;
-use Stats4sd\FilamentOdkLink\Filament\Resources\TeamResource;
-use Stats4sd\FilamentOdkLink\Models\OdkLink\XlsformTemplate;
 
 class FilamentOdkLinkPanelProvider extends PanelProvider
 {
@@ -28,8 +25,8 @@ class FilamentOdkLinkPanelProvider extends PanelProvider
             ->path('odk-link')
             ->login()
             ->default()
-            ->discoverResources(in: __DIR__ . "/Filament/Resources", for: 'Stats4sd\\FilamentOdkLink\\Filament\\Resources')
-            ->discoverPages(in: __DIR__ . "./Filament/Pages", for: 'Stats4sd\\FilamentOdkLink\\Filament\\Pages')
+            ->discoverResources(in: __DIR__ . '/Filament/Resources', for: 'Stats4sd\\FilamentOdkLink\\Filament\\Resources')
+            ->discoverPages(in: __DIR__ . './Filament/Pages', for: 'Stats4sd\\FilamentOdkLink\\Filament\\Pages')
             ->pages([
                 Dashboard::class,
             ])
