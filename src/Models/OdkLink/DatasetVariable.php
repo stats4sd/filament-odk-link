af0f16d29ee2adea7fdcc02810eb8b4d492d2646
Dave Mills <d.e.mills@stats4sd.org>
2024-03-05 15:45:29 +0000

    set proper relations for dataset_variables

--- src/Models/OdkLink/DatasetVariable.php
+++ src/Models/OdkLink/DatasetVariable.php
@@ -4,15 +4,33 @@ namespace Stats4sd\FilamentOdkLink\Models\OdkLink;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class DatasetVariable extends Model
 {
     protected $table = 'dataset_variables';
 
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
 
 
     public function dataset(): BelongsTo
     {
         return $this->belongsTo(Dataset::class);
     }
+
+    public function entities(): BelongsToMany
+    {
+        return $this->belongsToMany(Entity::class, 'entity_values')
+            ->using(EntityValue::class)
+            ->withPivot('value');
+    }
+
+    public function values(): HasMany
+    {
+        return $this->hasMany(EntityValue::class, 'entity_id');
+    }
+
 }
