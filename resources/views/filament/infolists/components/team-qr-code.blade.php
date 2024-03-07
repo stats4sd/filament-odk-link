291ac4eb107bd825e12ee911bd19304f79ed1e62
Dave Mills <d.e.mills@stats4sd.org>
2024-03-07 13:49:32 +0000

    qr code component should not crash when not linked to ODK Central

--- resources/views/filament/infolists/components/team-qr-code.blade.php
+++ resources/views/filament/infolists/components/team-qr-code.blade.php
@@ -1,4 +1,10 @@
+@if($getRecord()->odkProject)
 <div class="p-4">
     {{ QrCode::size(150)->generate($getRecord()->odkProject->appUsers->first()->qr_code_string) }}
         <h5>SCAN QR Code in ODK Collect</h5>
 </div>
+    @else
+    <div class="p-4">
+        <h5 class="text-center text-danger-600">ODK Project not found. Is this platform connected to a value ODK Central server?</h5>
+    </div>
+    @endif
