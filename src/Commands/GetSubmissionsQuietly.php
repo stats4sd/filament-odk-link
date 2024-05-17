97a2344f004cd6348d0066a88f18eb30da0a264b
Dave Mills <d.e.mills@stats4sd.org>
2024-05-17 15:37:07 +0100

    add command and job to get submissions quietly: - command gets submissions into the database without further processing. Useful for testing scenarios. - the job should be refactored to avoid code duplication. To be done after the current work on the data pulling

---
+++ src/Commands/GetSubmissionsQuietly.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Stats4sd\FilamentOdkLink\Commands;
+
+use Illuminate\Console\Command;
+use Stats4sd\FilamentOdkLink\Jobs\PullSubmissionsFromXlsformQuietly;
+use Stats4sd\FilamentOdkLink\Models\OdkLink\Xlsform;
+
+class GetSubmissionsQuietly extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:get-submissions-quietly';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Pulls submissions from ODK Central without running any additional processing. No Entities are created; no custom models are created. Mostly used for testing';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $xlsforms = Xlsform::where('is_active', true)->get()
+            ->each(function (Xlsform $xlsform) {
+                $this->info("Processing {$xlsform->title}...");
+                PullSubmissionsFromXlsformQuietly::dispatch($xlsform);
+            });
+    }
+}
+{
+
+}
---
+++ src/Jobs/PullSubmissionsFromXlsformQuietly.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Stats4sd\FilamentOdkLink\Jobs;
+
+class PullSubmissionsFromXlsformQuietly
+{
+
+
+}
