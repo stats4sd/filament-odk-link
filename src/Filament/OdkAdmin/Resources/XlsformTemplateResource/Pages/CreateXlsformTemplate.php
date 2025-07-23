5435e69dcf1b98c2aeb43f917f34da166cdd78fb
Dan Tang <dan@stats4sd.org>
2025-07-23 08:15:07 +0100

    WIP: add custom validation on type or_other

--- src/Filament/OdkAdmin/Resources/XlsformTemplateResource/Pages/CreateXlsformTemplate.php
+++ src/Filament/OdkAdmin/Resources/XlsformTemplateResource/Pages/CreateXlsformTemplate.php
@@ -2,15 +2,18 @@
 
 namespace Stats4sd\FilamentOdkLink\Filament\OdkAdmin\Resources\XlsformTemplateResource\Pages;
 
-use Filament\Forms\Components\Wizard;
-use Filament\Forms\Components\Wizard\Step;
-use Filament\Forms\Form;
 use Filament\Forms\Get;
+use Filament\Forms\Form;
+use Maatwebsite\Excel\Facades\Excel;
+use Filament\Forms\Components\Wizard;
+use Filament\Support\Exceptions\Halt;
 use Filament\Notifications\Notification;
+use Filament\Forms\Components\Wizard\Step;
 use Filament\Resources\Pages\CreateRecord;
-use Stats4sd\FilamentOdkLink\Filament\OdkAdmin\Resources\XlsformTemplateResource;
+use Stats4sd\FilamentOdkLink\Imports\XlsImport;
 use Stats4sd\FilamentOdkLink\Models\OdkLink\Platform;
 use Stats4sd\FilamentOdkLink\Models\OdkLink\XlsformTemplate;
+use Stats4sd\FilamentOdkLink\Filament\OdkAdmin\Resources\XlsformTemplateResource;
 
 class CreateXlsformTemplate extends CreateRecord
 {
@@ -45,6 +48,8 @@ class CreateXlsformTemplate extends CreateRecord
 
                     try {
 
+                        logger('CreateXlsformTemplate.getSteps()->afterValidation()...');
+
                         // wait to trigger the saved event until the xlsform file is attached.
 
                         // find the correct owner
@@ -58,7 +63,22 @@ class CreateXlsformTemplate extends CreateRecord
 
                         $xlsformTemplate->owner()->associate($owner);
 
+                        // TODO: check the uploaded excel file type column, throw error if it contains "or_other"
+
+
+                        // hardcode temporary for testing
+                        $unsupportedTypeFound = true;
+
+                        if ($unsupportedTypeFound) {
+                            throw new Halt('Type "or_other" is not recommended. Please do below updates on xlsform template and then try again. 1. Manually add an "Other" option to choices list. 2. Use a follow-up text question that is only relevant if "Other" is selected');
+                        }
+
+
+                        logger('before calling $xlsformTemplate->testOnOdkCentral()');
+
                         $xlsformTemplate = $xlsformTemplate->testOnOdkCentral();
+
+                        logger('after calling $xlsformTemplate->testOnOdkCentral()');
 
                         $xlsformTemplate->save();
 
