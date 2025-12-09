8b703c55457fcc8597d5121310b96fbe123f9a2f
Ciara <ciara@stats4sd.org>
2025-12-09 15:45:57 +0000

    fixes typo in migration

--- database/migrations/003_create_xlsform_modules_table.php
+++ database/migrations/003_create_xlsform_modules_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('xlsform_template_module_types');
+        Schema::dropIfExists('xlsform_template_modules');
     }
 };
