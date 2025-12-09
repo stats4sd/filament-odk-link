bd17575e33cc34a7432fcbdbf96197f216178f58
Ciara <ciara@stats4sd.org>
2025-12-09 15:30:12 +0000

    adds missing migration fields

--- database/migrations/003_create_xlsform_modules_table.php
+++ database/migrations/003_create_xlsform_modules_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('label');
             $table->string('name');
             $table->unsignedInteger('default_order')->nullable();
+            $table->boolean('can_be_extended')->default(false);
+            $table->boolean('can_be_replaced')->default(false);
+            $table->json('row_names')->nullable();
             $table->timestamps();
 
             $table->unique(['xlsform_template_id','name']);
