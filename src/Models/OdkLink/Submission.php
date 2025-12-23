a0570a6ee8c323779acc3828c49ebcfaa7106e0f
Dan Tang <dan@stats4sd.org>
2025-12-23 15:56:19 +0000

    add checking to $submission->primaryDataSubject

--- src/Models/OdkLink/Submission.php
+++ src/Models/OdkLink/Submission.php
@@ -80,7 +80,13 @@ class Submission extends Model implements HasMedia
             // if a submission is moved from test to live data, check and update the linked farm.
             if ($submission->isDirty('test_data')) {
                 $subject = $submission->primaryDataSubject;
-                $subject->updateCompletionStatus();
+
+                // ray($subject);
+
+                // Question: why $submission->primaryDataSubject is null?
+                if ($subject != null) {
+                    $subject->updateCompletionStatus();
+                }
             }
 
         });
